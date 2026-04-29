4edc4d5d805fc06744b978ec2339834803753d23
Jackie Valeri <jackievaleri8@gmail.com>
2026-04-29 14:00:05 -0400

    update metadata for supplemental scripts

--- src/S4B_interpret_compound_rationales_to_understand_activity.sh
+++ src/S4B_interpret_compound_rationales_to_understand_activity.sh
@@ -1,5 +1,33 @@
-# To use, conda activate chemprop and then bash [filename].sh
-# Note that to use the interpret function, needed to downgrade rdkit (as per https://github.com/chemprop/chemprop/issues/178): conda install -c conda-forge rdkit=2019.09.1
+# Interpret Compound Rationales to Understand Activity
+#
+# Overall Goal
+# - Run Chemprop's `interpret` over hit-predicted molecules to extract the
+#   rationale substructure (and substructure score) most responsible for each
+#   compound's predicted activity.
+#
+# Relation to Manuscript
+# - Produces the rationale text outputs visualized in Figure S4B by
+#   S4B_interpret_substructures_as_rationales_for_model_prediction.ipynb.
+#
+# Inputs
+# - Best D-MPNN ensemble: models/FINALbayHO04052022/ (PK + 37K final, from
+#   2D_train_final_models_all_data.sh).
+# - Per-round molecule sets in out/interpretation/mols_for_interpretation/:
+#     * 800k_mols_for_interpretation.csv (Broad 800K validation round)
+#     * molport_val1_mols_for_interpretation.csv (Molport round 1)
+#     * molport_val2_val3_mols_for_interpretation.csv (Molport rounds 2 + 3)
+#
+# Run Configuration
+# - chemprop_interpret with --property_id 1 (predicted-hit class), rationale
+#   size 8-10 atoms, --prop_delta 0.1, rdkit_2d_normalized features, no scaling.
+# - Output is redirected to per-round .txt files in
+#   out/interpretation/interpretation_results/.
+#
+# Prerequisites
+# - conda activate chemprop and run as: bash [filename].sh
+# - To use chemprop_interpret, RDKit must be downgraded
+#   (see https://github.com/chemprop/chemprop/issues/178):
+#   conda install -c conda-forge rdkit=2019.09.1
 
 export MODEL_PATH=../models/FINALbayHO04052022/;
 export DATA_PATH=../out/interpretation/mols_for_interpretation/;
